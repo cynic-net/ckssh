f5269d025b45b9df659313baa592c48b96d0c787
Curt J. Sampson <cjs@cynic.net>
2018-05-19 16:21:32 +0900

    Proof of concept that we can have separate output stream for eval
    We want `ckssh.py` to be able to print to stdout, print to stderr, and
    entirely independently of that return shell commands to the caller for
    it to execute. (This is so that it can, e.g., set environment
    variables in the calling shell's environment.)
    
    To do this we offer an `--eval-file` option to which ckssh.py can
    write code it would like the caller to execute. (If not provided, the
    code is written to stdout.)
    
    The `--proof-of-concept` opton shows that this works by printing text
    to both stdout and stderr and printing an `echo` command and
    environment variable setting to the eval-file which is then executed
    by the `ckset()` shell function.

--- bin/ckssh.py
+++ bin/ckssh.py
@@ -1,16 +1,48 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+from sys import stdout, stderr
 
+
+EVALFILE = stdout
+
+def evalwrite(s):
+    EVALFILE.write(s)
+    EVALFILE.write('\n')
+    
 def print_bash_init():
-    print('ckset() { true; }')
+    evalwrite('''
+        ckset() {
+            local retval=0;
+            local evalfile=$(mktemp -t ckssh-eval-XXXXX);
+            ckssh.py --eval-file "$evalfile" "$@"; retval=$?;
+            eval $(cat "$evalfile");
+            rm -f "$evalfile";
+            return $retval;
+        }
+    ''')
+
+def proof_of_concept():
+    print('stdout')
+    print('stderr', file=stderr)
+    evalwrite('echo evaled;')
+    evalwrite('export CKSSH_PROOF_OF_CONCEPT=1;')
 
 def main():
     p = ArgumentParser(description='Comparmentalized Key Agents for SSH')
     arg = p.add_argument
     arg('--bash-init', action='store_true',
         help='Print code to configure Bash with ckssh commands')
+    arg('--eval-file')
+    arg('--proof-of-concept', action='store_true')
     args = p.parse_args()
+
+    if args.eval_file:
+        global EVALFILE
+        EVALFILE = open(args.eval_file, 'wt')
+    if args.proof_of_concept:
+        proof_of_concept()
+        exit(0)
     if args.bash_init:
         print_bash_init()
         exit(0)
