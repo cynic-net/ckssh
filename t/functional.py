2b04267918ee9c48998bd24b2885ddb129ea65e0
Curt J. Sampson <cjs@cynic.net>
2018-11-26 08:32:56 +0900

    t/functional: Rework how we call the functions under test
    Instead of hacking our own parsed args object, we use ckssh's argument
    parser. We duplicate the subcommand lookup rather than using it
    directly because ckssh's main() does a sys.ext(); we should look for a
    way to reduce this duplication.
    
    This new method also consolidates most of the (fake) environment
    settings in one place. This means we're now running with a test
    environment with $HOME and $XDG_RUNTIME_DIR settings when we weren't
    before, but this should be ok, at least for the moment.

--- t/functional.py
+++ t/functional.py
@@ -1,34 +1,47 @@
-from    collections import namedtuple as ntup
+''' These "functional" tests go beyond unit tests in that they test
+    things like argument parsing and output to `EVALFILE`.
+
+    They do not call main() itself, run the code in a separate
+    interpreter, or check that Bash code from EVALFILE is correctly
+    executed by Bash. That's done to some degree by a minimal set of
+    true functional tests in the top-level ``Test`` script.
+'''
 from    io import StringIO
 from    pathlib import Path
 import  ckssh
 
-Args = ntup('TestArgs', 'params, a')
-Args.__new__.__defaults__ = ([], False)
+def main(subcommand, *, sockpath=None):
+    ''' Run something similar to ckssh.main() in the test framework.
+        Note that this runs in the existing process.
+        EVALFILE is set to a fresh StringIO() so its output can be asserted.
+    '''
+    ckssh.EVALFILE = StringIO()
+    TESTDIR = Path(__file__).parent
+    TESTHOME = str(Path(TESTDIR, 'mock_home'))
+    ENV = dict(
+        HOME=TESTHOME,
+        XDG_RUNTIME_DIR='/test-xdg-rtdir-nonexistent',
+        )
+    if sockpath: ENV['SSH_AUTH_SOCK'] = sockpath
+    p, subcommands = ckssh.argparser()
+    args = p.parse_args([subcommand])
+    return subcommands[args.subcommand](args, ENV)
 
 def test_print_bash_init():
-    ckssh.EVALFILE = StringIO()
-    ckssh.print_bash_init(None, {})
+    main('bash-init')
     assert 'ckset()' in ckssh.EVALFILE.getvalue()
 
 def test_ckset_show_no_compartment(capsys):
-    ckssh.ckset(Args(), {})
+    main('ckset')
     cap = capsys.readouterr()
     assert ('', 'ckssh: No compartment.\n') == (cap.out, cap.err)
 
-TESTDIR = Path(__file__).parent
-TESTHOME = str(Path(TESTDIR, 'mock_home'))
-
 def test_ckset_show_unknown_compartment(capsys):
-    ENV = dict(SSH_AUTH_SOCK='/not/known')
-    ckssh.ckset(Args(), ENV)
+    main('ckset', sockpath='/not/known')
     cap = capsys.readouterr()
     assert ('', 'ckssh: Unknown compartment.\n') == (cap.out, cap.err)
 
 def test_ckset_show_compartment_name(capsys):
-    ENV = dict(HOME=TESTHOME,
-               XDG_RUNTIME_DIR='/test-xdg-rtdir-nonexistent',
-               SSH_AUTH_SOCK='/test-xdg-rtdir-nonexistent/ckssh/socket/empty')
-    ckssh.ckset(Args(), ENV)
+    main('ckset', sockpath='/test-xdg-rtdir-nonexistent/ckssh/socket/empty')
     cap = capsys.readouterr()
     assert ('empty\n', '') == (cap.out, cap.err)
