2c025265cf26cb258cb97a275da58a4006807416
Curt J. Sampson <cjs@cynic.net>
2018-12-04 23:02:08 +0900

    Implement basic configuration file parsing
    Issues:
    1. This doesn't yet handle `CK_Host` directives.
    2. The error handling still needs improvement.
    3. The code for taking only the first value if a parameter is
       specified multiple times is a bit awkward not scalable.

--- bin/ckssh.py
+++ bin/ckssh.py
@@ -1,5 +1,85 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
+from    __future__ import print_function
+import  os, re
 
-print('Hello.')
+def ssh_bool(s):
+    ''' Parse an ssh_config-style boolean value.
+
+        This isn't really defined anywhere, as far as I can tell
+        (the manpages just use `yes` and `no`, though I'm sure that
+        `true` and `false` are allowed), so I just pick a reasonable
+        range of words that cannot be confused with some other kind
+        of value (e.g., I don't accept 0 as false).
+    '''
+    if s is True :                      return True
+    if s is False:                      return False
+    if s.lower() in ('yes', 'true'):    return True
+    if s.lower() in ('no', 'false'):    return False
+    raise Config.ConfigError('Invalid boolean value: {}'.format(s))
+
+class Compartment():
+    ' The configuration for a single compartment. '
+
+    def __init__(self, name):
+        self.name = name
+        self.keyfiles = []
+        self._confirm = None
+
+    def confirm(self):
+        ' Do we request confirmation when loading key into agent? '
+        if self._confirm is None:
+            return True             # unset
+        return self._confirm
+
+    def set_confirm(self, value):
+        ''' Set confirm value if not already set.
+            This accepts bool or an ssh_config-syntax string value.
+        '''
+        b = ssh_bool(value)
+        if self._confirm is None:
+            self._confirm = b
+
+class Config():
+    ' The compartments and other configuration read from the config file. '
+
+    class ConfigError(RuntimeError):
+        pass
+
+    @staticmethod
+    def load(input):
+        parser = re.compile(r'(?:\s*)(\w+)(?:\s*=\s*|\s+)(.+)')
+        conf = Config()
+        compartments = conf.compartments
+        curname = None
+        for line in input:
+            match = parser.match(line)
+            if not match: continue
+            key = match.group(1).lower(); value = match.group(2)
+            if key == 'ck_compartment':
+                curname = value
+                compartments.setdefault(curname, Compartment(curname))
+                continue
+            if key == 'ck_keyfile':
+                compartments[curname].keyfiles.append(value)
+                continue
+            if key == 'ck_confirm':
+                compartments[curname].set_confirm(value)
+                continue
+            if key == 'ck_host' or key == 'ck_compartmentname':
+                #   Currently unimplemented.
+                curname = None
+                continue
+            if key.startswith('ck_'):
+                raise Config.ConfigError(
+                    'Unknown config parameter: {}'.format(key))
+            # All non-CK parameters are ignored.
+        return conf
+
+    def __init__(self):
+        self.compartments = {}
+
+if __name__ == '__main__':
+    with open(os.path.expanduser('~/.ssh/ckssh_config')) as f:
+        conf = Config.load(f)
+    print(sorted(conf.compartments.keys()))
--- t/unit.py
+++ t/unit.py
@@ -1,4 +1,91 @@
 ' Unit Tests '
 
-def test_nothing():
-    assert True
+from    io import StringIO
+from    os.path import dirname, join as opj
+import  pytest
+from    ckssh import ssh_bool, Compartment, Config
+
+TESTDIR = dirname(__file__)
+CONFIGFILE = opj(TESTDIR, 'mock_home/.ssh/ckssh_config')
+
+def test_ssh_bool():
+    for t in ['yes', 'true', 'trUE', True]:
+        assert True is ssh_bool(t)
+    for f in ['no', 'false', False]:
+        assert False is ssh_bool(f)
+    with pytest.raises(Config.ConfigError):
+        ssh_bool('0')
+
+def test_compartment():
+    comp = Compartment('t')
+    assert 't' == comp.name
+    assert []  == comp.keyfiles
+    assert True is comp.confirm()
+
+    comp.set_confirm('no');
+    assert False is comp.confirm()
+
+    comp.set_confirm(True)
+    assert False is comp.confirm()  # First setting takes precedence
+
+    with pytest.raises(RuntimeError):
+        comp.set_confirm('unknown')
+
+def test_Config_load_empty():
+    conf  = Config.load(StringIO())
+    assert {} == conf.compartments
+
+def test_Config_load():
+    input = u'''
+        CK_Compartment alice
+        CK_Compartment bob@example.com
+            CK_Keyfile /tmp/id_temp
+            CK_Confirm false
+            #   First value takes precedence
+            CK_Confirm true
+            SomeOtherParam ignored
+        CK_Compartment charlie
+        CK_Compartment bob@example.com
+            CK_Keyfile ~/.ssh/id_rsa
+    '''
+    conf  = Config.load(StringIO(input))
+    assert ['alice', 'bob@example.com', 'charlie'] \
+        == sorted(conf.compartments.keys())
+
+    alice = conf.compartments['alice']
+    assert 'alice'              == alice.name
+    assert []                   == alice.keyfiles
+    assert True                 is alice.confirm()
+
+    bob = conf.compartments['bob@example.com']
+    assert 'bob@example.com'    == bob.name
+    assert '/tmp/id_temp'       == bob.keyfiles[0]
+    assert '~/.ssh/id_rsa'      == bob.keyfiles[1]
+    assert 2                    == len(bob.keyfiles)
+    assert False                is bob.confirm()
+
+def test_Config_load_unknown_ckparam():
+    input = u'''
+        # Blank lines to check that line number of error is correct
+        CK_ThisIsntAKnownParam false
+        '''
+    with pytest.raises(Config.ConfigError) as e:
+        Config.load(StringIO(input))
+    assert e.match('Unknown config parameter')
+
+@pytest.mark.xfail(strict=True, desc='We need to write error handling.')
+def test_Config_load_keywithoutvalue():
+    with pytest.raises(Config.ConfigError) as e:
+        Config.load(StringIO(u'CK_this_is_wrong'))
+    assert e.match(r'^Unknown config parameter on line 1: ck_this_is_wrong$')
+
+@pytest.mark.xfail(strict=True, desc='We need to write error handling.')
+def test_Config_load_errors():
+    #   Compartment config before a compartment name is given.
+    Config.load(StringIO(u'CK_Confirm false'))
+
+def test_t_config_file():
+    with open(CONFIGFILE) as f:
+        conf = Config.load(f)
+    assert '~/.ssh/cjs@cynic.net-120531' \
+        == conf.compartments['cjs@cynic.net'].keyfiles[1]
